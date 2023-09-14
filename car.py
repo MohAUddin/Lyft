9582fe56e619a0366b926b28ff4d33b2bd81640c
MohAUddin <mohammad.uddin454@gmail.com>
2023-09-14 00:11:00 -0400

    added comments to code to know what they do

--- car.py
+++ car.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 
-# cooment to see if it works
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, last_service_date): # __init__ is known as a constructor and is called when a new object of a class is  created. Allows you to inlitalize attributes of an object So any car that is added or chosen with the class Car will have the varaibels listed
+        self.last_service_date = last_service_date   # sets the current last service data for the self to the one identified in the varaibles
 
-    @abstractmethod
+    @abstractmethod # way of creating special permissions or an area to place special permissions without defining them but making sure that they are used in the code lateron
     def needs_service(self):
         pass
+
